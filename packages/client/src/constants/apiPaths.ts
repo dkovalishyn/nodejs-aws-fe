8e3da14f61c182435f6df60ac271d00bb6fd1217
Denys Kovalishyn <Denys_Kovalishyn@epam.com>
2020-12-05 22:05:13 +0200

    feat: publish to EB stage

--- packages/client/src/constants/apiPaths.ts
+++ packages/client/src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
-
 const API_PATHS = {
-  product: 'https://inv4v0ipye.execute-api.eu-central-1.amazonaws.com/dev',
-  order: 'https://inv4v0ipye.execute-api.eu-central-1.amazonaws.com/dev',
-  import: 'https://6yqkrrthgc.execute-api.eu-central-1.amazonaws.com/dev',
-  bff: 'https://inv4v0ipye.execute-api.eu-central-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: "https://inv4v0ipye.execute-api.eu-central-1.amazonaws.com/dev",
+  order: "https://inv4v0ipye.execute-api.eu-central-1.amazonaws.com/dev",
+  import: "https://6yqkrrthgc.execute-api.eu-central-1.amazonaws.com/dev",
+  bff: "https://inv4v0ipye.execute-api.eu-central-1.amazonaws.com/dev",
+  cart:
+    "http://dkovalishyn-cart-api-develop.eu-central-1.elasticbeanstalk.com/",
 };
 
 export default API_PATHS;
