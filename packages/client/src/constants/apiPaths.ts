39f423748964b413c105c3b2b719f57c3cf664d0
Denys Kovalishyn <Denys_Kovalishyn@epam.com>
2020-12-05 22:26:07 +0200

    feat: update client

--- packages/client/src/constants/apiPaths.ts
+++ packages/client/src/constants/apiPaths.ts
@@ -3,7 +3,8 @@ const API_PATHS = {
   product: 'https://inv4v0ipye.execute-api.eu-central-1.amazonaws.com/dev',
   order: 'https://inv4v0ipye.execute-api.eu-central-1.amazonaws.com/dev',
   import: 'https://inv4v0ipye.execute-api.eu-central-1.amazonaws.com/dev',
-  bff: 'https://inv4v0ipye.execute-api.eu-central-1.amazonaws.com/dev'
+  bff: 'https://inv4v0ipye.execute-api.eu-central-1.amazonaws.com/dev',
+  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
